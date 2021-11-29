b86e8db6760323d9df42f1e940e999f0eefb5378
saga0619 <june992@gmail.com>
2021-11-29 17:10:25 +0900

    Add comment on rtnet shell script

--- rtnet_script/start_rtnet.sh
+++ rtnet_script/start_rtnet.sh
@@ -1,18 +1,29 @@
 #!/bin/bash
 
+# To start rtnet, type start_rtnet.sh start.
+# To stop rtnet, type start_rtnet.sh stop.
+
+# -------------------------------
+# ------- Configuration  --------
+# Pci address of NIC (ex: 0000:03:00.0)
+#   Pci address can be checked with lspci
 devices="0000:03:00.0 0000:03:00.1 0000:04:00.0 0000:04:00.1"
+
+# default loaded driver on the NIC.
+#   List of drivers that can be used on the NIC, can be checked with 'lspci -v' command on terminal. (ex: kernel modules : igb, rt_igb)
 driver="igb"
+
+# RT driver to use with the NIC (usually comes with 'rt_' letters are in front of driver name)
 rtdriver="rt_igb"
+# ------ Configuration End ------
+# -------------------------------
+
 
 case "$1" in
     start)
-
         modprobe rtpacket
-
         dev_num=0
-
         for dev in $devices; do
-
             if [ -d /sys/bus/pci/devices/$dev/driver ]; then
             echo $dev unbind $driver
 	    echo -n $dev > /sys/bus/pci/drivers/$driver/unbind
@@ -25,15 +36,10 @@ case "$1" in
         done
 	;;
     stop)
-
         rmmod rtpacket
-
         dev_num=0
-
         for dev in $devices; do
-
             rtifconfig rteth$dev_num down
-
             echo 1 > /sys/bus/pci/devices/$dev/remove
             echo removing $dev to $rtdriver
             dev_num=$(($dev_num + 1))
